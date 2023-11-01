94d943b02da17b534060b8f376a1931f6f975e5b
jakelyons01 <jakelyons01@gmail.com>
2023-11-01 16:53:00 -0400

    now redirects to menu page when accessing root

---
+++ src/pages/menu.js
@@ -0,0 +1,14 @@
+/* 
+ * menu.js
+ */
+
+import GameViewer from "./index"; 
+
+export default function Menu() {
+
+    return (
+        <main>
+            {"lorem ipsum\n pee pee poo poo"}
+        </main>
+    );
+}
