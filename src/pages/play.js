0b99f4196f287840f7f9b54a84567548a4f3c388
jakelyons01 <jakelyons01@gmail.com>
2023-11-01 20:19:01 -0400

    moved over Kuskus and Julia's code to play.js

--- src/pages/play.js
+++ src/pages/play.js
@@ -2,15 +2,40 @@
  * menu.js
  */
 import { useRouter } from "next/router";
-import MapDisplay from "@/components/MapDisplay";
+import React, { useEffect, useState } from "react";
+import MapDisplay from "../components/MapDisplay";
+import { Traversal } from "../components/Traversal";
 
 export default function GameViewer() {
   const router = useRouter();
-  const mapMock = [Array.from("abc"), Array.from("def"), Array.from("ghi")];
+  const initialMap = [Array.from("abc"), Array.from("def"), Array.from("ghi")];
+
+  const [currentMap, setCurrentMap] = useState(initialMap);
+  const [position, setPosition] = useState([
+    Math.floor(currentMap.length / 2),
+    Math.floor(currentMap[0].length / 2),
+  ]);
+  // eslint-disable-next-line no-console
+  console.log(position);
+
+  useEffect(() => {
+    function handleKeyPress(event) {
+      setPosition(Traversal(currentMap, event.key, position));
+      // eslint-disable-next-line no-console
+      console.log(position);
+      setCurrentMap(currentMap);
+    }
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [currentMap, position]);
 
   return (
     <main>
-      <MapDisplay currentMap={mapMock} />
+      <MapDisplay currentMap={currentMap} />
       <button type="button" onClick={() => router.back()}>
         Quit
       </button>
