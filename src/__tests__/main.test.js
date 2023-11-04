d7b0abfc775813d7cdfaacf956bce8418672d36c
jakelyons01 <jakelyons01@gmail.com>
2023-11-04 16:30:27 -0400

    added GameViewer test

--- src/__tests__/main.test.js
+++ src/__tests__/main.test.js
@@ -2,6 +2,7 @@ import { render } from "@testing-library/react";
 import mockRouter from "next-router-mock";
 import { createDynamicRouteParser } from "next-router-mock/dynamic-routes";
 import CamelsHump from "@/pages/index";
+import GameViewer from "@/pages/play";
 
 // eslint-disable-next-line global-require
 jest.mock("next/router", () => require("next-router-mock"));
@@ -16,5 +17,8 @@ mockRouter.useParser(
 describe("End-to-end testing", () => {
   test("Render index.js component", () => {
     render(<CamelsHump />);
+  });
+  test("Render GameViwer page", () => {
+    render(<GameViewer />);
   });
 });
