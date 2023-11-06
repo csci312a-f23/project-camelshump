8a02e327f317b568f8e5529139ec979ee19b6d71
Ethan Lavallee <elavallee@middlebury.edu>
2023-11-05 19:53:20 -0500

    text box is formatted correctly with map display

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,54 +1,53 @@
 /* eslint-disable react/jsx-no-bind */
-import { HfInference } from '@huggingface/inference'
-import {useState} from "react";
+import { HfInference } from "@huggingface/inference";
+import { useState } from "react";
 import TextPrompt from "./TextPrompt";
+import styles from "../styles/TextBox.module.css";
 
+const hf = new HfInference("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");
 
+export default function TextBox() {
+  const [generatedText, setGeneratedText] = useState("");
 
-const hf = new HfInference("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")
+  const genKwargs = {
+    max_new_tokens: 128,
+    top_k: 30,
+    top_p: 0.9,
+    temperature: 0.2,
+    repetition_penalty: 1.02,
+    stopSequences: ["\nUser:", "<|endoftext|>", "</s>"],
+  };
 
-export default function TextBox(){
-    const [generatedText, setGeneratedText] = useState("");
-
-    const genKwargs = {
-        max_new_tokens: 256,
-        top_k: 30,
-        top_p: 0.9,
-        temperature: 0.2,
-        repetition_penalty: 1.02,
-        stopSequences: ['\nUser:', '<|endoftext|>', '</s>'],
+  async function getText(question) {
+    setGeneratedText();
+    let textStream = "";
+    const stream = await hf.textGenerationStream({
+      model: "tiiuae/falcon-7b-instruct",
+      inputs: question,
+      parameters: genKwargs,
+    });
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const r of stream) {
+      if (r.token.special) {
+        // eslint-disable-next-line no-continue
+        continue;
       }
+      // stop if we encounter a stop sequence
+      if (genKwargs.stopSequences.includes(r.token.text)) {
+        break;
+      }
+      textStream += r.token.text;
+    }
+    setGeneratedText(textStream);
+    // console.log(generatedText);
+  }
 
-    async function getText(question){
-        setGeneratedText();
-        let textStream = "";
-        const stream  =  await hf.textGenerationStream({
-            model: 'tiiuae/falcon-7b-instruct',
-            inputs: question,
-            parameters: genKwargs,
-          })
-        // eslint-disable-next-line no-restricted-syntax
-        for await (const r of stream){
-            if (r.token.special) {
-                // eslint-disable-next-line no-continue
-                continue
-              }
-              // stop if we encounter a stop sequence
-              if (genKwargs.stopSequences.includes(r.token.text)) {
-                break
-              }
-              textStream += r.token.text;
-        }
-        setGeneratedText(textStream);
-        // console.log(generatedText);
-    };
-
-
-    return (
-    <div >
+  return (
+    <div>
+      <div className={styles.textBox}>
         <p>{generatedText}</p>
-        <TextPrompt getText={getText}/>
+      </div>
+      <TextPrompt getText={getText} />
     </div>
-    )
-
+  );
 }
