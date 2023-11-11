f4c356ce48f7cf38c1aaf275053613edf21ad23c
Zoe Greenwald <zgreenwald@middlebury.edu>
2023-11-11 12:38:46 -0500

    styling class select

--- src/pages/classSelect.js
+++ src/pages/classSelect.js
@@ -13,13 +13,32 @@ export default function ClassSelector() {
   return (
     <main className="gridContainer">
       <div className="warriorContainer" onClick={() => handleClick("warrior")}>
-        <div>Warrior</div>
+        <div>
+          Warrior
+          <br />
+          <br />
+          Warrior class begins with a sword. This is a basic class with high
+          attack and defense who loves to fight.
+        </div>
       </div>
       <div className="mageContainer" onClick={() => handleClick("mage")}>
-        <div>Mage</div>
+        <div>
+          Mage
+          <br />
+          <br />
+          Mage is a class that starts with a staff. This class specializes in
+          magic and spells. These include attack and healing.
+        </div>
       </div>
       <div className="rogueContainer" onClick={() => handleClick("rogue")}>
-        <div>Rogue</div>
+        <div>
+          Rogue
+          <br />
+          <br />
+          Rogue is a class that starts with a knife. This class specializes in
+          avoiding being detected. Although it does not have strong attack or
+          defense, it cannot be blocked.
+        </div>
       </div>
     </main>
   );
