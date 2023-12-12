0963b3fa0e1ea73ebc5bd89691df01fdb26796fc
jakelyons01 <jakelyons01@gmail.com>
2023-12-12 14:41:10 -0500

    changed map update when temoving items from map to not mutate props

--- src/pages/play.js
+++ src/pages/play.js
@@ -471,8 +471,10 @@ export default function GameViewer({ className }) {
       await getText(
         `I am a fantasy character. I just found a ${pickup}, describe what I see.`,
       );
-      currentMap[position[2]][position[0]][position[1]] = "-";
-      setCurrentMap(currentMap);
+
+      const newMapItem = [...currentMap];
+      newMapItem[position[2]][position[0]][position[1]] = "-";
+      setCurrentMap(newMapItem);
     }
   };
 
